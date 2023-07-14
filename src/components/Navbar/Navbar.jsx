91a9e62a00ba05809f15179785b46ab30349ad35
digimori <hanaorikuu@gmail.com>
2023-07-14 17:56:57 +0000

    feat: Hamburger menu for small screens - Need to implement Media query for it to correctly function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { giHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import images from '../../constants/images';
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.gericht} alt="logo" />
+        <a href="/"><img src={images.gericht} alt="logo" /></a>
       </div>
       <ul className='app__navbar-links'>
         <li className='p__opensans'><a href="#home">Home</a></li>
@@ -18,6 +18,24 @@ const Navbar = () => {
         <li className='p__opensans'><a href="#awards">Awards</a></li>
         <li className='p__opensans'><a href="#contact">Contact</a></li>
       </ul>
+      <div className='app__navbar-login'>
+        <a href="#login" className='p__opensans'>Log in / Register</a>
+        <div />
+        <a href="/" className='p__opensans'>Book Table</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+        <div className='app__navbar-smallscreen_overlay flex-center slide-bottom'>
+        <MdOutlineRestaurantMenu className='overlay__close' fontsize={27} onClick={() => {}} />
+          <ul className='app__navbar-smallscreen-links'>
+          <li className='p__opensans'><a href="#home">Home</a></li>
+          <li className='p__opensans'><a href="#about">About</a></li>
+          <li className='p__opensans'><a href="#menu">Menu</a></li>
+          <li className='p__opensans'><a href="#awards">Awards</a></li>
+          <li className='p__opensans'><a href="#contact">Contact</a></li>
+        </ul>
+          </div> 
+      </div>
     </nav>
   )
 }
