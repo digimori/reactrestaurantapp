bbf95f848e29c58bed84fb859785f7a2f28c388b
digimori <hanaorikuu@gmail.com>
2023-07-17 18:37:07 +0000

    feat: SpecialMenu component added, divs rendered containing mapped data and images. - Currently unstyled

--- src/container/SpecialMenu/SpecialMenu.jsx
+++ src/container/SpecialMenu/SpecialMenu.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+
+import { SubHeading, MenuItem } from '../../components';
+
+import { images, data } from '../../constants';
+import './SpecialMenu.css';
 
 const SpecialMenu = () => {
   return (
-    <div>SpecialMenu</div>
+    <div class="app__specialmenu flex__center section__padding" id="menu">
+      <div className='app__specialmenu--title'>
+        <SubHeading title="Menu that fits your palette" />
+        <h1 className='headtext__cormorant'>Today's Special</h1>
+      </div>
+
+      <div className='app__specialmenu-menu'>
+      <div className='app__specialmenu-menu_wine flex__center'>
+        <p className="app__specialmenu-menu_heading">Wine & Beer</p>
+        <div className='app__specialmenu-menu_items'>
+          {data.wines.map((wine, index) =>(
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className='app__specialmenu-menu_img'>
+        <img src={images.menu} alt="menu" />
+      </div>
+
+      <div className='app_specialmenu-menu_cocktails flex__center'>
+        <p className='app__specialmenu-menu_heading'>Cocktails</p>
+        <div className='app__specialmenu-menu_items'>
+        {data.cocktails.map((cocktail, index) =>(
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+      </div>
+      <div className='app__specialmenu-btn' style={{marginTop: '15px'}}>
+        <button type="button" className='custom__button'>View More</button>
+      </div>
+      </div>
   )
 }
 
