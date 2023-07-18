f4822b85ce98496e4735f0b1db7cd133e33e242d
digimori <hanaorikuu@gmail.com>
2023-07-18 12:57:37 +0000

    feat: Gallery component created - Divs and buttons implemented - No styling - Scroll logic added to buttons

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+import './Gallery.css';
 
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if(direction === 'left') {
+      current.scrollLeft -=300;
+    } else {
+      current.scrollLeft +=300;
+    }
+  }
+
   return (
-    <div>Gallery</div>
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title="Instagram" />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem'}}></p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+
+    <div className='app__gallery-images'>
+      <div className='app__gallery-images_container' ref={scrollRef}></div>
+      <div className='app__gallery-images_arrows'>
+        <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
+        <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+      </div>
+    </div>
+
+    </div>
   )
 }
 
