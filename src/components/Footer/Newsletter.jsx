c574eaada9def04346934b3cfac0b0d88272311f
digimori <hanaorikuu@gmail.com>
2023-07-18 13:47:08 +0000

    feat: Newsletter section added with components - To be styled

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import SubHeading from '../Subheading/SubHeading'
+import './Newsletter.css';
 
 export const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <div className='app__newsletter'>
+      <div className='app__newsletter-heading'>
+        <Subheading title="Newsletter" />
+        <h1 className='headtext__cormorant'>Subscribe to our Newsletter</h1>
+        <p className='p__opensans'>Never miss our latest updates!</p>
+      </div>
+      <div className='app__newsletter-input flex__center'>
+        <input type="email" placeholder='Enter your Email' />
+        <button className='custom__button'>Subscribe</button>
+      </div>
+    </div>
   )
 }
 
