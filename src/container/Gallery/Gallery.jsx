6c7529947bcec1c4c4a7384d0cdf742421d8bf30
digimori <hanaorikuu@gmail.com>
2023-07-18 13:05:23 +0000

    style: Images mapped into Gallery.jsx component - Unstyled

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -18,6 +18,8 @@ const Gallery = () => {
     }
   }
 
+  const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
   return (
     <div className='app__gallery flex__center'>
       <div className='app__gallery-content'>
@@ -28,15 +30,21 @@ const Gallery = () => {
       </div>
 
     <div className='app__gallery-images'>
-      <div className='app__gallery-images_container' ref={scrollRef}></div>
+      <div className='app__gallery-images_container' ref={scrollRef}>
+        {galleryImages.map((image, index) => (
+          <div class="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+            <img src={image} alt="gallery" />
+            <BsInstagram className='gallery__image-icon' />
+          </div>
+        ))}
+        </div>
       <div className='app__gallery-images_arrows'>
         <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
         <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
       </div>
-    </div>
-
-    </div>
-  )
-}
+      </div>
+      </div>
+  );
+};
 
 export default Gallery
